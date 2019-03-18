d3ccf46cec8931b9595ca9a75ed6595c0a974a12
Abhishek Jaiswal <jaiswala@usc.edu>
2019-03-18 12:06:39 -0700

    First commit, data loading complete with props

--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import Table from './Table';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+
+        const dynamictable = [
+            {
+                'name': 'Bob',
+                'job': 'Manager'
+            },
+            {
+                'name': 'Mohsen',
+                'job': 'Senior Dev'
+            },
+            {
+                'name': 'Wyatt',
+                'job': 'Senior Dev'
+            },
+            {
+                'name': 'Abhishek',
+                'job': 'Junior Dev'
+            }
+        ];
+        return (
+
+            <div className="container">
+                {/* <h1>Helloooooooo! React is here!</h1> */}
+                <Table tabularData = {dynamictable} />
+            </div>
+
+        );
+    }
 }
 
 export default App;
---
+++ src/Table.js
@@ -0,0 +1,85 @@
+import React, {Component} from 'react';
+
+class Table extends Component {
+    render() {
+        const {tabularData} = this.props;
+        return(
+            <table>
+                {/* <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Job</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Bob</td>
+                        <td>Manager</td>
+                    </tr>
+                    <tr>
+                        <td>Mohsen</td>
+                        <td>Senior Dev</td>
+                    </tr>
+                    <tr>
+                        <td>Wyatt</td>
+                        <td>Senior Dev</td>
+                    </tr>
+                    <tr>
+                        <td>Abhishek</td>
+                        <td>Junior Dev</td>
+                    </tr>
+                </tbody> */}
+
+                <TableHeader />
+                <TableBody tabularData = {tabularData} />
+
+            </table>
+        );
+    }
+}
+
+export default Table;
+
+
+const TableHeader = () => {
+    return(
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Job</th>
+            </tr>
+        </thead>
+    );
+}
+
+const TableBody = props => {
+    const rows = props.tabularData.map((row, index) => {
+        return (
+            <tr key={index}>
+                <td>{row.name}</td>
+                <td>{row.job}</td>
+            </tr>
+        );
+    });
+    return(
+        <tbody>
+            {rows}
+            {/* <tr>
+                <td>Bob</td>
+                <td>Manager</td>
+            </tr>
+            <tr>
+                <td>Mohsen</td>
+                <td>Senior Dev</td>
+            </tr>
+             <tr>
+                <td>Wyatt</td>
+                <td>Senior Dev</td>
+             </tr>
+            <tr>
+                <td>Abhishek</td>
+                <td>Junior Dev</td>
+             </tr> */}
+        </tbody>
+    );
+}
